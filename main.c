5102c257ae3470cd3caceac00a603f38b32de136
Suzanne Baltar <C15Catherine.Baltar@C15CSBaltar.usafa.ds.af.edu>
2013-10-27 17:07:41 -0600

    need screen 1 to return

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ char MessageSize2 = 15;
 char Message2[]="Of all time.   ";
 char Message3[]="Message?";
 char Message4[]="Press123.";
-char Message5[]="Not really.    ";
+char Message5[]="I love it.     ";
 char MessageSize5=15;
 char Message6[]="Sorry.  ";
 char MessageSize6=8;
@@ -32,18 +32,23 @@ int main(void) {
 
     P1DIR |= BIT0|BIT6;                             // set launchpad LEDs to output
 
+    Line1();
+    write(Message3, 8);
+    Line2();
+    write(Message4,8);
+    _delay_cycles(100000);
+
     while (1) {
 
-    	write(Message3, 8);
-    	    	Line2();
-    	    	write(Message4,8);
-    	    	_delay_cycles(100000);
 
         char buttons[] = {BIT1, BIT2, BIT3};
         char pressedButton = pollP1Buttons(buttons, 3);
 
+
         switch (pressedButton) {
-            case BIT1:
+
+          case BIT1:
+            	Line1();
             	write(Message1, 8);
             	    	Line2();
             	    	write(Message2,8);
@@ -55,17 +60,20 @@ int main(void) {
             	    	LCDclear();// do something in response
                 break;
             case BIT2:
+            	Line1();
             	write(Message1, 8);
-            	    	Line2();
+            	Line2();
             	    	write(Message5,8);
             	    	_delay_cycles(100000);
 
             	    	scroll(Message1, MessageSize1);
             	    	scroll(Message5, MessageSize5);
 
-            	    	LCDclear();// do something in response
+            	    	LCDclear();
+
                 break;
             case BIT3:
+            	Line1();
             	write(Message1, 8);
             	    	Line2();
             	    	write(Message6,8);
@@ -75,24 +83,21 @@ int main(void) {
             	    	scroll(Message6, MessageSize6);
 
             	    	LCDclear();// toggle LEDs and wait for release
-                P1OUT ^= BIT0|BIT6;
-                waitForP1ButtonRelease(BIT3);
                 break;
 
-    while(1)
-    {
-    	write(Message1, 8);
-    	Line2();
-    	write(Message2,8);
-    	_delay_cycles(100000);
+            case BIT1 & BIT2 & BIT3:
+            	  Line1();
+            	  write(Message3, 8);
+            	  Line2();
+            	  write(Message4,8);
+            	  _delay_cycles(100000);
+            	break;
+        		}
 
-    	scroll(Message1, MessageSize1);
-    	scroll(Message2, MessageSize2);
-
-    	LCDclear();
-    }
-
-	return 0;
+        }
 }
+
+
+
 
 
