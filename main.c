6aa1f566034132c3e2494f93d59989a97f4220fb
Suzanne Baltar <C15Catherine.Baltar@C15CSBaltar.usafa.ds.af.edu>
2013-10-27 17:27:41 -0600

    bFunctionality

--- main.c
+++ main.c
@@ -6,13 +6,19 @@ char MessageSize1 = 31;
 char Message1[]="ECE 382 is my favorite class!  ";
 char MessageSize2 = 15;
 char Message2[]="Of all time.   ";
+char Message3[]="Message?";
+char Message4[]="Press123.";
+char Message5[]="I love it.     ";
+char MessageSize5=15;
+char Message6[]="Sorry.  ";
+char MessageSize6=8;
 
 
 /*
  * main.c
  */
 // a comment
-// b func comment
+
 int main(void) {
    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
@@ -22,20 +28,71 @@ int main(void) {
     LCDclear();
 
    //Write to LCD
+    configureP1PinAsButton(BIT1|BIT2|BIT3);         // configure pins 1, 2, and 3 as buttons
+
+    P1DIR |= BIT0|BIT6;                             // set launchpad LEDs to output
+
+    Line1();
+    write(Message3, 8);
+    Line2();
+    write(Message4,8);
+    _delay_cycles(100000);
+
+    while (1) {
 
 
-    while(1)
-    {
-    	write(Message1, 8);
-    	Line2();
-    	write(Message2,8);
-    	_delay_cycles(100000);
+        char buttons[] = {BIT1, BIT2, BIT3};
+        char pressedButton = pollP1Buttons(buttons, 3);
 
-    	scroll(Message1, MessageSize1);
-    	scroll(Message2, MessageSize2);
 
-    	LCDclear();
-    }
+        switch (pressedButton) {
 
-	return 0;
+          case BIT1:
+            	Line1();
+            	write(Message1, 8);
+            	    	Line2();
+            	    	write(Message2,8);
+            	    	_delay_cycles(100000);
+
+            	    	scroll(Message1, MessageSize1);
+            	    	scroll(Message2, MessageSize2);
+
+            	    	LCDclear();// do something in response
+                break;
+            case BIT2:
+            	Line1();
+            	write(Message1, 8);
+            	Line2();
+            	    	write(Message5,8);
+            	    	_delay_cycles(100000);
+
+            	    	scroll(Message1, MessageSize1);
+            	    	scroll(Message5, MessageSize5);
+
+            	    	LCDclear();
+
+                break;
+            case BIT3:
+            	Line1();
+            	write(Message1, 8);
+            	    	Line2();
+            	    	write(Message6,8);
+            	    	_delay_cycles(100000);
+
+            	    	scroll(Message1, MessageSize1);
+            	    	scroll(Message6, MessageSize6);
+
+            	    	LCDclear();// toggle LEDs and wait for release
+                break;
+
+            case BIT1 & BIT2 & BIT3:	//if none are pressed it will display:
+            	  Line1();
+            	  write(Message3, 8);
+            	  Line2();
+            	  write(Message4,8);
+            	  _delay_cycles(100000);
+            	break;
+        		}
+
+        }
 }
