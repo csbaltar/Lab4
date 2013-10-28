950e2d994ae86c26944e9fa8ada52206e1f21827
Suzanne Baltar <C15Catherine.Baltar@C15CSBaltar.usafa.ds.af.edu>
2013-10-28 14:41:55 -0600

    done

--- lcd.h
+++ lcd.h
@@ -11,7 +11,6 @@
 
 
 //Capt Branchflower's functions
-void INITSPI();
 void LCDclear();
 void LCDinit();
 void writeCommandNibble(char commandNibble);
@@ -21,17 +20,81 @@ void LCD_write_8(char byteToSend);
 void SPI_send(char byteToSend);
 //End Capt Brachflower's functions
 
+/*Function: initSPI
+ * Author: Suzanne Baltar
+ * Description: initializes the lcd, sets bits
+ * Documentation:  based off code from Lab 3, revieved help from Capt Brachflower.
+ */
 void initSPI();
+
+/*Function: delayMilli
+ * Author: Suzanne Baltar
+ * Description: delays for 1.65 milliseconds, or 1776 clock cycles
+ * Documentation: none
+ */
 void delayMilli();
+
+/*Function: delayMicro
+ * Author: Suzanne Baltar
+ * Description: delays for 40 microseconds, or 44 clock cycles
+ * Documentation: none
+ */
 void delayMicro();
+
+/*Function: set_SS_lo
+ * Author: Suzanne Baltar
+ * Description: sets slave select low
+ * Documentation: none
+ */
 void set_SS_lo();
+
+/*Function: set_SS_hi
+ * Author: Suzanne Baltar
+ * Description: sets slave select high
+ * Documentation: none
+ */
 void set_SS_hi();
+
+/*Function: LCD_write_4
+ * Author: Suzanne Baltar
+ * Description: function
+ * Documentation: based off code from lab 3
+ */
 void LCD_write_4(unsigned char sendByte);
+
+/*Function: writeChar
+ * Author: Suzanne Baltar
+ * Description: writes characters
+ * Documentation: based off code from lab 3
+ */
 void writeChar(char ascii);
+
+/*Function: Line1
+ * Author: Suzanne Baltar
+ * Description: puts cursor to line 1
+ * Documentation: none
+ */
 void Line1();
+
+/*Function: Line2
+ * Author: Suzanne Baltar
+ * Description: put cursor on line 2
+ * Documentation: none
+ */
 void Line2();
 
+/*Function: write
+ * Author: Suzanne Baltar
+ * Description: writes messages to screen based on message size
+ * Documentation: revieved help from Derek Titus with ending the loop.
+ */
 void write(char Message[], char MessageSize);
+
+/*Function: scroll
+ * Author: Suzanne Baltar
+ * Description: scrolls though the message based on message size
+ * Documentation: Derek Titus helped me with using message size.
+ */
 void scroll(char Message[], char MessageSize);
 
 
